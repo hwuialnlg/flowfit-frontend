256c876ad8936f15dbc6d57f2066fdd5548a72a0
Will Huang <96509348+hwuialnlg@users.noreply.github.com>
2024-12-16 22:09:22 -0800

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,31 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from '@mui/material/Box';
-import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import { AppBar, IconButton, Stack, Toolbar, Typography } from "@mui/material";
 import { useNavigate } from "react-router";
 import { Person } from "@mui/icons-material"
 
 export default function NavBar() {
     const navigate = useNavigate();
+    const [pages, setPages] = useState(["Exercises"])
     return (
         <AppBar position={'static'}>
             <Toolbar sx={{display: 'flex', justifyContent: 'space-between'}}>
-                <Typography
-                    variant="h6"
-                    noWrap
-                    component="a"
-                    onClick={() => navigate("/")}
-                    sx={{
-                        mr: 2,
-                        display: { xs: 'none', md: 'flex' },
-                        fontFamily: 'monospace',
-                        fontWeight: 700,
-                        letterSpacing: '.3rem',
-                        color: 'inherit',
-                        textDecoration: 'none',
-                    }}
-                >
-                    FlowFit
-                </Typography>
+                <Stack flexDirection={'row'}  sx={{display: 'flex'}} columnGap={2}>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        onClick={() => navigate("/")}
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        FlowFit
+                    </Typography>
+
+                    {
+                        pages.map((page) => {
+                            return (
+                                <Typography alignContent={'center'} textAlign={'center'}>{page}</Typography>
+                            )
+                        })
+                    }
+                </Stack>
                 <IconButton edge='end'
                     onClick={() => navigate("/login")}
                 >
